142ec11f327daf03f7774420cd811abe54a825e6
vann4oto98 <vanyoivanov98@gmail.com>
2023-05-05 14:16:54 +0300

    Initial Commit

---
+++ ethereal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ethereal
+//
+//  Created by Vanyo Ivanov on 5.05.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ethereal/etherealApp.swift
@@ -0,0 +1,17 @@
+//
+//  etherealApp.swift
+//  ethereal
+//
+//  Created by Vanyo Ivanov on 5.05.23.
+//
+
+import SwiftUI
+
+@main
+struct etherealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
